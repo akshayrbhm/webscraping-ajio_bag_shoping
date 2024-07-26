b91ff820ba44ad8cd438673cf740384c8b46f482
Akshay Bhivagade <akshayrbhm@gmail.com>
2024-07-26 15:36:44 +0530

    first commit

---
+++ ajio.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+import time
+
+s = Service('C:/Users/aksha/Desktop/chromedriver.exe')
+
+driver = webdriver.Chrome(service=s)
+driver.get('https://www.ajio.com/men-backpacks/c/830201001')
+time.sleep(1)
+
+old_height = driver.execute_script('return document.body.scrollHeight')
+counter = 1
+while True:
+
+    driver.execute_script('window.scrollTo(0, document.body.scrollHeight)')
+    time.sleep(2)
+
+    print(counter)
+    counter +=1
+    print(old_height)
+
+    new_height = driver.execute_script('return document.body.scrollHeight')
+    print(new_height)
+    if new_height==old_height:
+        break
+    old_height = new_height
+
+
+html = driver.page_source
+with open('ajio.html','w', encoding='utf-8') as f:
+    f.write(html)
+# print(height)
+time.sleep(100)
